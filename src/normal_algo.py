d33bf916f10e13cf5898794b91e2d370f525031e
st087217 <st0872172@student.spbu.ru>
2020-09-19 22:27:44 +0300

    объявила переменные

--- src/normal_algo.py
+++ src/normal_algo.py
@@ -1,5 +1,19 @@
 word = input()
 n = int(input())
-word1 = [0] * n
-word2 = [0] * n
-for i in range (n)
+word1 = [""] * n
+word2 = [""] * n
+last = [0] * n
+for i in range(n):
+    str = input()
+    str = str.split(",")
+    word1[i] = str[0]
+    word2[i] = str[1]
+    last[i] = int(str[2])
+while i < n:
+    if (word.find(word1) != -1) and (last[i] != 0):
+        word = word.replace(word1[i], word2[i], 1)
+        i = 0
+    if last[i] = 0:
+        word = word.replace(word1[i], word2[i], 1)
+        break
+print(word)
