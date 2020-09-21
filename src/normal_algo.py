93b422b7ca0564709276f4a3bcad2caca19578fa
st087217 <st0872172@student.spbu.ru>
2020-09-21 23:01:35 +0300

    написала код

--- src/normal_algo.py
+++ src/normal_algo.py
@@ -1,21 +1,26 @@
-file_way =
-with (file_way, 'r') as file:
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+args = parser.parse_args()
+file = args.file
+with (file, 'r') as file:
     word = file.readline()
     n = int(file.readline())
-    word1 = [""] * n
-    word2 = [""] * n
-    last = [0] * n
-for i in range(n):
-    str = input()
-    str = str.split(",")
-    word1[i] = str[0]
-    word2[i] = str[1]
-    last[i] = int(str[2])
+    word_before = [""] * n
+    word_after = [""] * n
+    last_string = [0] * n
+    for i in range(n):
+        string = input()
+        string = string.split(",")
+        word_before[i] = string[0]
+        word_after[i] = string[1]
+        last_string[i] = int(string[2])
 while i < n:
-    if (word.find(word1) != -1) and (last[i] != 0):
-        word = word.replace(word1[i], word2[i], 1)
+    if (word.find(word_before) != -1) and (last_string[i] != 0):
+        word = word.replace(word_before[i], word_after[i], 1)
         i = 0
-    if last[i] == 0:
-        word = word.replace(word1[i], word2[i], 1)
+    if last_string[i] == 0:
+        word = word.replace(word_before[i], word_after[i], 1)
         break
 print(word)
