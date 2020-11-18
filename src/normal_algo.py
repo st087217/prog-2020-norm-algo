02c6443f9791a37545d8d0e3d25f34b15d280d3f
st087217 <st0872172@student.spbu.ru>
2020-11-18 20:02:08 +0300

    функция, применяющая марковскую подстановку к входной строке

--- src/normal_algo.py
+++ src/normal_algo.py
@@ -13,3 +13,26 @@ def parse_args():
     return parser.parse_args()
 
 
+class Rule:
+    left = ""
+    right = ""
+    final = False
+
+
+NON_FINAL_DELIMITER = "=>"
+FINAL_DELIMITER = "=>."
+EPSILON_SYMBOL = "ε"
+
+
+def apply_rule(rule, input):
+    """ Функция, которая применяет правило к входной строке """
+
+    if rule.left == EPSILON_SYMBOL:
+        return True, rule.right + input
+
+    if rule.left in input:
+        if rule.right == EPSILON_SYMBOL:
+            return True, input.replace(rule.left, "", 1)
+        return True, input.replace(rule.left, rule.right, 1)
+
+    return False, input
