001568b8f3c1ce0b184c71a20e82606b0e44c7cc
st087217 <st0872172@student.spbu.ru>
2020-11-18 19:14:31 +0300

    функция, принимающая аргументы из командной строки

--- src/normal_algo.py
+++ src/normal_algo.py
@@ -1,28 +1,15 @@
 import argparse
+import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file")
-args = parser.parse_args()
-file = args.file
-with (file, 'r') as file:
-    word = file.readline()
-    n = int(file.readline())
-    word_before = [""] * n
-    word_after = [""] * n
-    last_string = [0] * n
-    for i in range(n):
-        string = input()
-        string = string.split(",")
-        word_before[i] = string[0]
-        word_after[i] = string[1]
-        last_string[i] = int(string[2])
-while i < n:
-    if (word.find(word1) != -1) and (last[i] != 0)
-        if (word.find(word_before) != -1) and (last_string[i] != 0):
-        word = word.replace(word_before[i], word_after[i], 1)
-        i = 0
-        if last_string[i] == 0:
-        word = word.replace(word_before[i], word_after[i], 1)
-        break
 
-print(word)
+def parse_args():
+    """ Функция, которая принимает аргументы из командной строки(путь к файлу со схемой
+    марковских подстановок, путь к файлу со словом, путь к файлу, в который нужно вывести слово,
+    полученное в результате применения марковских подсатновок)"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rules", type=argparse.FileType("r"), required=True)
+    parser.add_argument("--input", type=argparse.FileType("r"), required=True)
+    parser.add_argument("--output", type=argparse.FileType("w"), default=sys.stdout)
+    return parser.parse_args()
+
+
